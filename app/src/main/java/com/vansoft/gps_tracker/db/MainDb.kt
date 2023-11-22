3eb1e65dd9de4a639273e5f871adc8b89167ec05
Ivan2000-web <vany6408678@gmail.com>
2023-11-22 21:38:24 +0300

    add: MainDb class (Room)

---
+++ app/src/main/java/com/vansoft/gps_tracker/db/MainDb.kt
@@ -0,0 +1,25 @@
+package com.vansoft.gps_tracker.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TrackItem::class], version = 1)
+abstract class MainDb : RoomDatabase() {
+    companion object{
+        @Volatile
+        var INSTANCE: MainDb? = null
+        fun getDatabase(context: Context): MainDb{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MainDb::class.java,
+                    "GpsTracker.db"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
